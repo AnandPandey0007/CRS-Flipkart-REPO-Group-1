63c6d3d1df1c74bca22d8af53ce0b3ca33f2cc8f
Anup Krishna Kumar <anup.kumar11@BLRETV-L7M0DFPQ.local>
2022-06-21 14:03:58 +0530

    professorImpl

--- CRS-Group-1-Flipkart-POS/src/com/flipkart/services/ProfessorInterfaceImpl.java
+++ CRS-Group-1-Flipkart-POS/src/com/flipkart/services/ProfessorInterfaceImpl.java
@@ -2,6 +2,7 @@ package com.flipkart.services;
 
 import com.flipkart.beans.EnrolledStudent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProfessorInterfaceImpl implements ProfessorInterface {
@@ -13,11 +14,19 @@ public class ProfessorInterfaceImpl implements ProfessorInterface {
 
     @Override
     public List<EnrolledStudent> viewEnrolledStudents(String profId) {
-        return null;
+        List<EnrolledStudent> enrolledStudents=new ArrayList<EnrolledStudent>();
+        EnrolledStudent st1= new EnrolledStudent("1","C1",1);
+        enrolledStudents.add(st1);
+        EnrolledStudent st2= new EnrolledStudent("1","C1",2);
+        enrolledStudents.add(st2);
+        EnrolledStudent st3= new EnrolledStudent("1","C1",2);
+        enrolledStudents.add(st3);
+        return enrolledStudents;
     }
 
     @Override
     public boolean addGrade(int studentId, String courseCode, String grade) {
-        return false;
+            System.out.println("The grade of the studentId : " +studentId+" has been updated to "+grade);
+            return true;
     }
 }
