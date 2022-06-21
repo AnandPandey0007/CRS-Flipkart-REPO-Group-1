9514e8e3f6c9dbca5a9e35d0fe1c0cd3892e17fe
Anubhuti Srivastava <anubhuti.s@flipkart.com>
2022-06-21 14:37:38 +0530

    student impl

--- CRS-Group-1-Flipkart-POS/src/com/flipkart/services/StudentInterfaceImpl.java
+++ CRS-Group-1-Flipkart-POS/src/com/flipkart/services/StudentInterfaceImpl.java
@@ -2,54 +2,91 @@ package com.flipkart.services;
 
 import com.flipkart.beans.Course;
 import com.flipkart.beans.Grade;
+import com.flipkart.beans.Student;
 import com.flipkart.constant.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StudentInterfaceImpl implements StudentInterface{
 
+    Map<String, Student> students;
+    Map<String, List<Course>> courses;
+
+
+    public void createStudent(){
+        students = new HashMap<>(); courses = new HashMap<>();
+    }
     @Override
     public int register(String name, String userID, String password, Gender gender, int batch, String branch, String address, String country) {
+        Student newStudent = new Student();
+        newStudent.setStudentName(name);
+        newStudent.setStudentId(userID);
+        students.put(userID, newStudent);
         return 0;
     }
 
     @Override
-    public int getStudentId(String userId) {
-        return 0;
+    public String getStudentId(String userId) {
+        return userId;
     }
 
     @Override
-    public boolean isApproved(int studentId) {
-        return false;
+    public boolean isApproved(String userID) {
+        return students.get(userID).isApproved();
     }
 
     @Override
-    public boolean addCourse(String courseCode, int studentId, List<Course> studentCourseList) {
-        return false;
+    public boolean addCourse(String userID, List<Course> studentCourseList) {
+        List<Course> existingCourseList;
+        if(students.get(userID).getCourses() == null){
+            existingCourseList = new ArrayList<>();
+        } else {
+            existingCourseList = new ArrayList<>(students.get(userID).getCourses());
+        }
+        for(Course newCourse : studentCourseList){
+            existingCourseList.add(newCourse);
+        }
+        courses.put(userID, existingCourseList);
+        return true;
     }
 
     @Override
-    public boolean dropCourse(String courseCode, int studentId, List<Course> studentCourseList) {
-        return false;
+    public boolean dropCourse(String courseCode, String studentId, List<Course> studentCourseList) {
+        List<Course> enrolledCourses;
+        if(students.get(studentId).getCourses() == null){
+            return false;
+        } else {
+            enrolledCourses = students.get(studentId).getCourses();
+        }
+        for(Course enrolled : enrolledCourses) {
+            if(enrolled.getCourseCode().equals(courseCode)){
+                enrolledCourses.remove(enrolled);
+            }
+        }
+        return true;
     }
 
     @Override
-    public List<Course> viewCourses(int studentId) {
-        return null;
+    public List<Course> viewCourses(String studentId) {
+        return students.get(studentId).getCourses();
     }
 
     @Override
-    public void setRegistrationStatus(int studentId) {
-
+    public void setRegistrationStatus(String studentId) {
+         students.get(studentId).setApproved(true);
     }
 
     @Override
-    public boolean getRegistrationStatus(int studentId) {
-        return false;
+    public boolean getRegistrationStatus(String studentId) {
+        return students.get(studentId).isApproved();
     }
-
+//
     @Override
     public List<Grade> viewGradeCard(int studentId) {
         return null;
     }
+
 }
